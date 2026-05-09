84cf47c56344d4db3b7e52b96b6d95fdfd0f81e1
Manith991 <rithsaramanith123@gmail.com>
2026-05-09 22:57:03 +0700

    using package google_fonts, local font (assets font) and change flutter app name

---
+++ lib/02_views/20_eshop/view/home_view.dart
@@ -0,0 +1,388 @@
+import 'package:flutter/material.dart';
+import 'package:my_flutter_codes/02_views/20_eshop/view/product_detail.dart';
+import 'package:my_flutter_codes/02_views/constant_root.dart';
+
+import '../component/custome_app.dart';
+import 'cart_view.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int selectedIndex = 0;
+  final categories = ["All", "Smartphones", "Headphones", "Laptops"];
+  var lstProducts = [
+    {
+      "name": "AirPods",
+      "imageUrl": "assets/images/iphone14.webp",
+      "price": "1320",
+      "rateCount": 4.9,
+    },
+    {
+      "name": "MacBook",
+      "imageUrl": "assets/images/iphone14.webp",
+      "price": "1250",    
+      "rateCount": 4.5,
+    },
+    {
+      "name": "MacBook",
+      "imageUrl": "assets/images/iphone14.webp",
+      "price": "1100",
+      "rateCount": 5.0,
+    },
+    {
+      "name": "MacBook",
+      "imageUrl": "assets/images/iphone14.webp",
+      "price": "1500",
+      "rateCount": 4.6,
+    },
+    {
+      "name": "MacBook",
+      "imageUrl": "assets/images/iphone14.webp",
+      "price": "1300",
+      "rateCount": 4.9,
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: k20BackgroundColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: ListView(
+          children: [
+            CustomAppBar(),
+            const SizedBox(height: 10),
+
+            // Search bar
+            SizedBox(
+              height: 50,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Search",
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  filled: true,
+                  fillColor: k20FillColor,
+                  suffixIcon: const Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // Clearance sales banner
+            SizedBox(
+              height: 200,
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: k20ActiveColor,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  Positioned(
+                    left: 15,
+                    top: 0,
+                    bottom: 0,
+                    width: 180,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Clearance\nSales",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Poppins",
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 8,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: const Text(
+                            "%  Up to 50%",
+                            style: TextStyle(
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    right: 5,
+                    bottom: 0,
+                    top: -15,
+                    child: Image.asset(
+                      "assets/images/iphone14.png",
+                      fit: BoxFit.contain,
+                      alignment: Alignment.bottomRight,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 15),
+
+            // Categories header
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Categories",
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 22),
+                ),
+                Text(
+                  "See all",
+                  style: TextStyle(
+                    color: k20ActiveColor,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 10),
+
+            // Scrollable category chips
+            SizedBox(
+              height: 45,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                separatorBuilder: (_, __) => const SizedBox(width: 10),
+                itemBuilder: (context, index) {
+                  final isSelected = selectedIndex == index;
+                  return GestureDetector(
+                    onTap: () => setState(() => selectedIndex = index),
+                    child: Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      decoration: BoxDecoration(
+                        color: isSelected ? Colors.green : Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                        border: isSelected
+                            ? null
+                            : Border.all(color: Colors.black, width: 2),
+                      ),
+                      child: Text(
+                        categories[index],
+                        style: TextStyle(
+                          color: isSelected ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              height: 310,
+              child: GridView.builder(
+                itemCount: lstProducts.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.7,
+                  crossAxisSpacing: 12,
+                  mainAxisSpacing: 12,
+                ),
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => ProductDetail()),
+                      );
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xfff1f3f3),
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withValues(alpha: 0.05),
+                            blurRadius: 10,
+                            offset: Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // Product Image
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.all(10),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset(
+                                  lstProducts[index]['imageUrl'].toString(),
+                                  fit: BoxFit.contain,
+                                  width: double.infinity,
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          // Product Info
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade300,
+                              borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(20),
+                                bottomRight: Radius.circular(20),
+                              ),
+                            ),
+
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 12,
+                                vertical: 8,
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                        child: Text(
+                                          lstProducts[index]['name'].toString(),
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                      ),
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            Icons.star,
+                                            color: Colors.amber,
+                                            size: 16,
+                                          ),
+                                          SizedBox(width: 2),
+                                          Text(
+                                            lstProducts[index]['rateCount']
+                                                .toString(),
+                                            style: TextStyle(fontSize: 13),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+
+                                  SizedBox(height: 6),
+
+                                  // Price
+                                  Text(
+                                    '\$${lstProducts[index]['price']}',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: k20ActiveColor,
+                                    ),
+                                  ),
+
+                                  SizedBox(height: 5),
+
+                                  // Add to Cart Button
+                                  SizedBox(
+                                    width: double.infinity,
+                                    height: 35,
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: k20ActiveColor,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(
+                                            10,
+                                          ),
+                                        ),
+                                        padding: EdgeInsets.zero,
+                                      ),
+                                      onPressed: () => Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (_) => CartView(),
+                                        ),
+                                      ),
+                                      child: Text(
+                                        "Add to Cart",
+                                        style: TextStyle(
+                                          fontSize: 13,
+                                          color: k20BackgroundColor,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: CusBottomNav(),
+    );
+  }
+}
+
+class CusBottomNav extends StatelessWidget {
+  const CusBottomNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      backgroundColor: k20BackgroundColor,
+      type: BottomNavigationBarType.fixed,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite_border),
+          label: "Favorite",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.perm_identity),
+          label: "Profile",
+        ),
+      ],
+    );
+  }
+}
