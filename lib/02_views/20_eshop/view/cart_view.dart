aa701dee13d91060f0822e4707e55b64adfb4c84
Manith991 <rithsaramanith123@gmail.com>
2026-05-11 19:02:51 +0700

    using package google_fonts, local font (assets font) and change flutter app name

---
+++ lib/02_views/20_eshop/view/cart_view.dart
@@ -0,0 +1,345 @@
+import 'package:flutter/material.dart';
+import 'package:my_flutter_codes/02_views/constant_root.dart';
+const gap = SizedBox(height: 10,);
+class CartView extends StatefulWidget {
+  const CartView({super.key});
+
+  @override
+  State<CartView> createState() => _CartViewState();
+}
+
+class _CartViewState extends State<CartView> {
+  final List<CartItem> cartItems = [
+    CartItem(name: "Xbox Series Controller", color: "Black", price: 80),
+    CartItem(name: "PlayStation Controller", color: "White", price: 75),
+    CartItem(name: "Nintendo Switch Pro Controller", color: "Red", price: 70),
+    CartItem(name: "Xbox Series Controller", color: "Black", price: 80),
+    CartItem(name: "PlayStation Controller", color: "White", price: 75),
+    CartItem(name: "Nintendo Switch Pro Controller", color: "Red", price: 70),
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: k20BackgroundColor,
+      appBar: AppBar(
+        backgroundColor: k20BackgroundColor,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 14),
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withValues(alpha: 0.3),
+                  blurRadius: 6,
+                  offset: const Offset(2, 2),
+                ),
+              ],
+            ),
+            child: CircleAvatar(
+              backgroundColor: Colors.white,
+              child: IconButton(
+                onPressed: () => Navigator.pop(context),
+                icon: Icon(Icons.arrow_back_ios_new_outlined, size: 20),
+              ),
+            ),
+          ),
+        ),
+        title: const Text("My cart"),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 12),
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.black.withValues(alpha: 0.1),
+                  width: 1,
+                ),
+              ),
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.more_horiz),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: cartItems.length,
+                      itemBuilder: (context, index) {
+                        final item = cartItems[index];
+                        return Column(
+                          children: [
+                            CartCard(item: item),
+                            const Divider(thickness: 2, color: k20FillColor),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: 300,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              border: Border.all(color: Colors.grey, width: 2),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withValues(alpha: 0.1),
+                  blurRadius: 6,
+                  offset: Offset(2, 2),
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                      border: Border.all(color: k20FillColor, width: 2),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "ADJ3AK",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Promo-code applied",
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Icon(Icons.check_circle, color: Colors.green),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      _buildRow("Subtotal:", "\$800.00"),
+                      gap,
+                      _buildRow("Delivery Fee:", "\$5.00"),
+                      gap,
+                      _buildRow("Discount:", "40%"),
+                      gap,
+                      Divider(thickness: 2),
+                      gap,
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: k20ActiveColor,
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 12,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Checkout for",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                "\$480.00",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      gap,
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CartItem {
+  final String name, color;
+  final int price;
+
+  CartItem({required this.name, required this.color, required this.price});
+}
+
+class CartCard extends StatelessWidget {
+  final CartItem item;
+
+  const CartCard({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 140,
+      width: double.infinity,
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Container(
+              height: 120,
+              width: 120,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: k20FillColor,
+              ),
+              child: Image.network("https://pngimg.com/d/xbox_PNG101370.png"),
+            ),
+          ),
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 0, top: 12, bottom: 12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          "Xbox series Controller",
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      const Icon(Icons.close, color: Colors.grey),
+                    ],
+                  ),
+                  const Text("Black", style: TextStyle(color: Colors.grey)),
+                  const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "\$80.00",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 45,
+                            child: OutlinedButton(
+                              onPressed: () {},
+                              style: OutlinedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                side: const BorderSide(
+                                  color: Colors.grey,
+                                  width: 2,
+                                ),
+                                padding: const EdgeInsets.all(12),
+                              ),
+                              child: const Icon(Icons.remove),
+                            ),
+                          ),
+                          const SizedBox(width: 12),
+                          const Text(
+                            "1",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(width: 12),
+                          SizedBox(
+                            width: 45,
+                            child: OutlinedButton(
+                              onPressed: () {},
+                              style: OutlinedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                side: BorderSide(
+                                  color: k20ActiveColor,
+                                  width: 2,
+                                ),
+                                padding: const EdgeInsets.all(12),
+                              ),
+                              child: Icon(Icons.add, color: k20ActiveColor),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget _buildRow(String label, String value) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Text(label, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+      Text(value, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+    ],
+  );
+}
+
